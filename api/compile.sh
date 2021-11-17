41b85e796ca510b84eafe6a76562fa27921f9d55
Nick Ionata <nionata@ufl.edu>
2021-11-17 17:28:21 -0500

    fix stubs not loading

--- api/compile.sh
+++ api/compile.sh
@@ -4,3 +4,6 @@ set -e
 
 # Compile the javascript pb files and add them to the react native app
 protoc --proto_path=./ --plugin=../app/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=../app/api *.proto
+
+# Bable throws errors when trying to resolve the generated .ts files so we must change the extension
+for f in ../app/api/*.ts; do mv -- "$f" "${f%.ts}.tsx"; done
