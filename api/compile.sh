b9026342da5479d1e59429c37456bf4ca43190b9
Nicholas Ionata <nionata@ufl.edu>
2021-11-17 17:29:15 -0500

    convert app api protos to ts (#19)
    * convert app api protos to ts
    
    * update api readme
    
    * remove '@' from module resolver
    
    * fix stubs not loading
    
    * revert alias

--- api/compile.sh
+++ api/compile.sh
@@ -3,4 +3,7 @@
 set -e
 
 # Compile the javascript pb files and add them to the react native app
-protoc --proto_path=./ --js_out=import_style=commonjs,binary:../app/api *.proto
+protoc --proto_path=./ --plugin=../app/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=../app/api *.proto
+
+# Bable throws errors when trying to resolve the generated .ts files so we must change the extension
+for f in ../app/api/*.ts; do mv -- "$f" "${f%.ts}.tsx"; done
