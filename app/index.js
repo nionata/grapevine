b28f74ce4bdc8fb0c00d7b249ab33a6b13466bdb
Paul Le Grand <paul@paullegrand.org>
2021-11-17 17:42:59 -0500

    Update to latest module and remove extra fonts

--- app/index.js
+++ app/index.js
@@ -6,7 +6,4 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-// import { AppRegistry } from "react-native";
-// import App from "react-native-ble-manager/example/App"; //<-- simply point to the example js!
-
-AppRegistry.registerComponent("Yolo", () => App);
+AppRegistry.registerComponent(appName, () => App);
