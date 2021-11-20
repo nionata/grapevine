295f0b8725062ee4ccebb508ec7f9b2a69d82a44
Nick Ionata <nionata@ufl.edu>
2021-11-20 00:58:01 -0500

    Fixed app rendering and bluetooth issues

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,46 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import HomeScreen from './screens/home';
-import ScanScreen from './screens/scan';
-import AdvertiseScreen from 'screens/advertise';
-import SettingsScreen from './screens/settings';
+import HomeScreen from 'screens/home';
+import ScanScreen from 'screens/peers';
+import SettingsScreen from 'screens/settings';
+import BluetoothManager, { BluetoothMode } from 'bluetooth/manager'
+import { Message } from 'api/message'
 
 const Tab = createBottomTabNavigator();
 
-const App = () => {
-  // const isDarkMode = useColorScheme() === 'dark';
+interface AppProps {
+}
+interface AppState {
+  messages: Message[]
+}
 
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName: string = '';
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+  }
 
-            if (route.name === 'GrapeVine') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Scan' || route.name === 'Advertise') {
-              iconName = focused ? 'bluetooth' : 'bluetooth-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'cog' : 'cog-outline';
-            }
+  componentDidMount() {
+    const bluetoothManager = new BluetoothManager(
+      BluetoothMode.Advertise, 
+      () => this.state.messages,
+      (message: Message) => {
+        console.log(this.state.messages, message)
+        this.setState((state) => {
+          return {
+            messages: [...state.messages, message]
+          }
+        })
+      }
+    )
+    bluetoothManager.start()
+  }
 
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'blueviolet',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="GrapeVine" component={HomeScreen} />
-        <Tab.Screen name="Scan" component={ScanScreen} />
-        <Tab.Screen name="Advertise" component={AdvertiseScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-};
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName: string = '';
+  
+              if (route.name === 'GrapeVine') {
+                iconName = focused ? 'home' : 'home-outline';
+              } else if (route.name === 'Peers') {
+                iconName = focused ? 'bluetooth' : 'bluetooth-outline';
+              } else if (route.name === 'Settings') {
+                iconName = focused ? 'cog' : 'cog-outline';
+              }
+  
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'blueviolet',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen name="GrapeVine" children={() => <HomeScreen messages={this.state.messages}/>} />
+          <Tab.Screen name="Peers" component={ScanScreen} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
 
 export default App;
--- app/src/screens/home.tsx
+++ app/src/screens/home.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import { View, Card, Text } from 'react-native-ui-lib';
-import { Messages, Message } from 'api/message';
+import { Message } from 'api/message';
 
-const messages: Messages = {
-  messages: [
-    Message.fromJSON({
-      content: "Here's a message I collected. Grapevine is dope!",
-    }),
-    Message.fromJSON({
-      content:
-        "Here's another message I collected. I think Grapevine is okay, but we need to get more people on.",
-    }),
-    Message.fromJSON({
-      content: 'Nick, can you hear me?',
-    }),
-  ],
-};
-
-function HomeScreen() {
-  const messageCards = messages.messages.map((message, index) => (
+function HomeScreen(props: { messages: Message[] }) {
+  const messageCards = props.messages.map((message, index) => (
     <Card paddingV-5 paddingH-5 marginV-10 activeOpacity={1} key={index}>
       <Text>{message.content}</Text>
     </Card>
