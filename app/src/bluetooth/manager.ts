53984809586f4c874e9d8a08add5636b02838fa5
Nick Ionata <nionata@ufl.edu>
2021-11-19 22:53:51 -0500

    bluetooth manager and peripheral

---
+++ app/src/bluetooth/manager.ts
@@ -0,0 +1,31 @@
+import BluetoothPeripheral, { GetMessages, SetMessage } from 'bluetooth/peripheral'
+
+export enum BluetoothMode {
+  Scan,
+  Advertise,
+}
+
+export default class BluetoothManager {
+  mode: BluetoothMode
+  // central: BleManager
+  peripheral: BluetoothPeripheral
+
+  constructor(
+    mode: BluetoothMode, 
+    getMessages: GetMessages, 
+    setMessage: SetMessage,
+  ) {
+    this.mode = mode
+    // this.central = new BleManager()
+    this.peripheral = new BluetoothPeripheral(getMessages, setMessage)
+  }
+
+  async start() {
+    if (this.mode == BluetoothMode.Scan) {
+      // this.central.startScanning()
+    } else if (this.mode == BluetoothMode.Advertise) {
+      this.peripheral.startAdvertising()
+    }
+  }
+}
+
---
+++ app/src/bluetooth/peripheral.ts
@@ -0,0 +1,58 @@
+import Manager from 'react-native-peripheral/lib/Manager'
+import { Service, Characteristic } from 'react-native-peripheral'
+import { GRAPEVINE_SERVICE_UUID, MESSAGE_CHARACTERISTIC_UUID } from '../Const'
+import { Message, Messages } from 'api/message'
+import { toByteArray, fromByteArray } from 'base64-js'
+
+export type GetMessages = () => Message[]
+export type SetMessage = (message: Message) => void
+
+export default class BluetoothPeripheral {
+  manager: Manager
+  poweredOn: boolean
+
+  constructor(getMessages: GetMessages, setMessage: SetMessage) {
+    this.manager = new Manager()
+    this.poweredOn = false
+
+    const messageCharacteristic = new Characteristic({
+      uuid: MESSAGE_CHARACTERISTIC_UUID,
+      properties: ['read', 'write'],
+      permissions: ['readable', 'writeable'],
+      onReadRequest: async(offset?: number) => {
+        const byteArr = Messages.encode(
+          Messages.fromJSON({ messages: getMessages() })
+        ).finish()
+        return fromByteArray(byteArr)
+      },
+      onWriteRequest: async(value: string, offset?: number) => {
+        setMessage(Message.decode(toByteArray(value)))
+      }
+    })
+
+    const grapevineService = new Service({
+      uuid: GRAPEVINE_SERVICE_UUID,
+      characteristics: [messageCharacteristic]
+    })
+
+    this.manager.onStateChanged(state => {
+      if (state === 'poweredOn') {
+        this.manager.addService(grapevineService)
+        this.poweredOn = true
+      } else {
+        this.poweredOn = false
+      }
+    })
+  }
+
+  async startAdvertising(): Promise<boolean> {
+    if (!this.poweredOn) {
+      return false
+    }
+    await this.manager.startAdvertising({
+      name: 'Grapevine Peer',
+      serviceUuids: [GRAPEVINE_SERVICE_UUID],
+    })
+    return true
+  }
+}
