2c4c09b8e2d659cb089cb3e2b76e4febaa9ee605
Paul Le Grand <paul@paullegrand.org>
2021-11-21 23:43:37 -0500

    Style compose, add discard button

--- app/src/screens/compose.tsx
+++ app/src/screens/compose.tsx
@@ -1,74 +1,99 @@
-import React, { createRef, useEffect } from 'react';
+import React, { createRef, ForwardedRef } from 'react';
 import { StyleSheet, TextInput } from 'react-native';
 import Modal from 'react-native-modalbox';
 import { TextField, Button, View } from 'react-native-ui-lib';
 import { cleanUpSwearyString } from 'swears';
 
-const ComposeModal = React.forwardRef<Modal>((props, ref) => {
-  const [messageInput, setMessageInput] = React.useState<string>('');
+const ComposeModal = React.forwardRef<Modal>(
+  (props, ref: ForwardedRef<Modal>) => {
+    const inputRef = createRef<TextInput>();
+    const [messageInput, setMessageInput] = React.useState<string>('');
 
-  const onClose = () => {
-    console.log('Modal just closed');
-  };
-  const inputRef = createRef<TextInput>();
+    const onOpen = () => {
+      // Don't need this anymore with autofocus={true}
+      // // inputRef.current?.focus();
+    };
 
-  const onOpen = () => {
-    console.log('Modal just opened');
-  };
+    const requestClose = () => {
+      // This is so ugly but it works
+      (ref as React.RefObject<Modal>)?.current?.close();
+    };
 
-  const saveMessage: () => Promise<boolean> = async () => {
-    try {
-      if (messageInput) {
-        // Censor the message before we save it
-        const cleanedInput = cleanUpSwearyString(messageInput);
-        setMessageInput(cleanedInput);
+    const onClose = () => {
+      setMessageInput('');
+    };
 
-        // TODO: Send the message to Firestore
+    const saveMessage: () => Promise<boolean> = async () => {
+      try {
+        if (messageInput) {
+          // Censor the message before we save it
+          const cleanedInput = cleanUpSwearyString(messageInput);
+          setMessageInput(cleanedInput);
+
+          // TODO: Send the message to Firestore
+        }
+        return true;
+      } catch (e) {
+        console.error(e);
+        return false;
       }
-      return true;
-    } catch (e) {
-      console.error(e);
-      return false;
-    }
-  };
+    };
 
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    // Update the document title using the browser API
-    inputRef.current?.focus();
-  });
+    return (
+      <Modal
+        ref={ref}
+        style={styles.modal}
+        swipeToClose={true}
+        onOpened={onOpen}
+        onClosed={onClose}
+      >
+        <View style={styles.messageInputContainer}>
+          <TextField
+            autoFocus={true}
+            multiline={true}
+            ref={inputRef}
+            style={styles.messageInput}
+            value={messageInput}
+            placeholder="Share something through the grapevine..."
+            onChangeText={setMessageInput}
+          />
 
-  return (
-    <Modal
-      ref={ref}
-      style={styles.modal}
-      swipeToClose={true}
-      onClosed={onClose}
-      onOpened={onOpen}
-    >
-      <View padding-20 paddingT-200>
-        <TextField
-          ref={inputRef}
-          value={messageInput}
-          placeholder="Share something through the grapevine..."
-          onChangeText={setMessageInput}
-        />
+          <Button
+            marginT-10
+            backgroundColor="blueviolet"
+            label="Submit"
+            size={Button.sizes.medium}
+            onPress={saveMessage}
+          />
 
-        <Button
-          marginT-10
-          backgroundColor="blueviolet"
-          label="Submit"
-          size={Button.sizes.medium}
-          onPress={saveMessage}
-        />
-      </View>
-    </Modal>
-  );
-});
+          <Button
+            marginT-10
+            backgroundColor="lightgray"
+            label="Discard"
+            size={Button.sizes.medium}
+            onPress={requestClose}
+          />
+        </View>
+      </Modal>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   modal: {
-    // justifyContent: 'center',
+    alignItems: 'center',
+    width: undefined,
+    height: undefined,
+    flex: 1,
+    paddingTop: 100,
+  },
+  messageInputContainer: {
+    display: 'flex',
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  messageInput: {
+    justifyContent: 'center',
     alignItems: 'center',
   },
 });
