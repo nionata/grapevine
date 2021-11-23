52f2817ec24240d2afa1068b1b2d815aa58d6e39
Paul Le Grand <paul@paullegrand.org>
2021-11-23 13:10:17 -0500

    Show dates

--- app/src/screens/home.tsx
+++ app/src/screens/home.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Card, Text } from 'react-native-ui-lib';
 import { Message } from 'api/message';
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
+import { format } from 'date-fns';
 
 function HomeScreen(props: { messages: Message[] }) {
+  const messages = props.messages.map((message) => {
+    return {
+      ...message,
+      createdAt: format(new Date(message.createdAt), 'MMM d, yyyy h:m aaa'),
+    };
+  });
+
   return (
     <FlatList
-      data={props.messages}
+      data={messages}
       renderItem={({ item }) => (
         <Card paddingV-5 paddingH-5 marginH-10 marginV-10 activeOpacity={1}>
           <Text>{item.content}</Text>
+          <Text style={styles.date}>{item.createdAt}</Text>
         </Card>
       )}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  date: {
+    color: 'lightgray',
+  },
+});
 
 export default HomeScreen;
